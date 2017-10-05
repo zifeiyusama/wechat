a19e0c03b909f3fcd22a363677c9af779d0b5fb0
zifeiyu <zifeiyusama@gmail.com>
2017-10-05 11:48:30 +0800

    finish router transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<transition
+	 :name='transitionName'
   >
   	<router-view></router-view>
 	</transition>
@@ -8,6 +9,22 @@
 import animate from 'animate.css'
 export default{
 	data() {
+		return {
+			transitionName: ''
+		}
+	},
+	watch: {
+		'$route'(to, from) {
+			if(to.name === 'home' && from.name === null) {
+				this.transitionName = ''
+			}
+			else if (to.name === 'home') {
+				this.transitionName = 'slideOut'
+			}
+			else if (to.name === 'user') {
+				this.transitionName = 'slideIn'
+			}
+		}
 	}
 }
 </script>
@@ -32,5 +49,43 @@ body {
 	min-height: 17.92rem;
 	margin: auto;
 	background: #fff;
+}
+.slideIn-enter {
+	left: 10rem;
+	top: 0;
+}
+.slideIn-enter-to {
+	left: 0;
+	top: 0;
+}
+.slideIn-leave {
+	left: 0;
+	top: 0;
+}
+.slideIn-leave-to {
+	left: -10rem;
+	top: 0;
+}
+.slideIn-enter-active, .slideIn-leave-active {
+	transition: all .3s linear;
+}
+.slideOut-enter {
+	left: -10rem;
+	top: 0;
+}
+.slideOut-enter-to {
+	left: 0;
+	top: 0;
+}
+.slideOut-leave {
+	left: 0;
+	top: 0;
+}
+.slideOut-leave-to {
+	left: 10rem;
+	top: 0;
+}
+.slideOut-enter-active, .slideOut-leave-active {
+	transition: all .3s linear;
 }
 </style>
